8168697e9de7eee0ac1a4fea6f5ea44c2ae3a6c2
Todd Branchflower <todd.branchflower@gmail.com>
2013-11-11 16:52:22 -0700

    added opcode parsing, some function stubs

--- assembler.js
+++ assembler.js
@@ -64,30 +64,182 @@ function getInstructionType(opcode)
 	return instrType;
 }
 
+function getOpcode(instruction)
+{
+	var opcode = '';
+
+	instruction = instruction.toLowerCase();
+
+	switch(instruction)
+	{
+		case 'rrc':
+			opcode = '000';
+			break;
+		case 'swpb':
+			opcode = '001';
+			break;
+		case 'rra':
+			opcode = '010';
+			break;
+		case 'sxt':
+			opcode = '011';
+			break;
+		case 'push':
+			opcode = '100';
+			break;
+		case 'call':
+			opcode = '101';
+			break;
+		case 'reti':
+			opcode = '110';
+			break;
+		case 'jne':
+		case 'jnz':
+			opcode = '000';
+			break;
+		case 'jeq':
+		case 'jz':
+			opcode = '001';
+			break;
+		case 'jnc':
+		case 'jlo':
+			opcode = '010';
+			break;
+		case 'jc':
+		case 'jhs':
+			opcode = '011';
+			break;
+		case 'jn':
+			opcode = '100';
+			break;
+		case 'jge':
+			opcode = '101';
+			break;
+		case 'jl':
+			opcode = '110';
+			break;
+		case 'jmp':
+			opcode = '111';
+			break;
+		case 'mov':
+			opcode = '0100';
+			break;
+		case 'add':
+			opcode = '0101';
+			break;
+		case 'addc':
+			opcode = '0110';
+			break;
+		case 'subc':
+			opcode = '0111';
+			break;
+		case 'sub':
+			opcode = '1000';
+			break;
+		case 'cmp':
+			opcode = '1001';
+			break;
+		case 'dadd':
+			opcode = '1010';
+			break;
+		case 'bit':
+			opcode = '1011';
+			break;
+		case 'bic':
+			opcode = '1100';
+			break;
+		case 'bis':
+			opcode = '1101';
+			break;
+		case 'xor':
+			opcode = '1110';
+			break;
+		case 'and':
+			opcode = '1111';
+			break;
+	}
+
+	return opcode;
+}
+
+function binaryToHex(binary)
+{
+	return parseInt(binary, 2).toString(16);
+}
+
+function joinInstructionComponents(assembledObj)
+{
+	var assembledInstr;
+
+	return assembledInstr;
+}
+
+function parseInstruction(instruction)
+{
+	instruction = instruction.toLowerCase();
+
+	instruction = instruction.split(' ');
+
+	instruction[0] = instruction[0].split('.');
+
+	return instruction;
+}
+
+function parseSingle(instruction)
+{
+	var assembledObj = {};
+
+	instruction = parseInstruction(instruction);
+
+	assembledObj.opcode = getOpcode(instruction[0][0]);
+
+	console.log(assembledObj);
+
+	return assembledObj;
+}
+
+function parseDouble(instruction)
+{
+	var assembledObj = {};
+
+	instruction = parseInstruction(instruction);
+
+	assembledObj.opcode = getOpcode(instruction[0][0]);
+
+	console.log(assembledObj);
+
+	return assembledObj;
+}
+
+// need to know information about offset to determine this...
+function parseJump(instruction)
+{
+}
+
 function assembleInstruction(instruction)
 {
+	parsedInstruction = parseInstruction(instruction);
 
-	var splitInstruction = instruction.split(' ');
-
-	var splitOpcode = splitInstruction[0].split('.');
-
-	if (isEmulatedInstruction(splitOpcode[0])
+	/*
+	if (isEmulatedInstruction(parseInstruction[0][0])
 			instruction = getActualInstructionFromEmulated(instruction);
+	 */
 
-	switch(getInstructionType(splitOpcode[0]))
+	switch(getInstructionType(parsedInstruction[0][0]))
 	{
 		case 'single':
-			console.log('single');
+			parseSingle(instruction);
 			break;
 		case 'double':
-			console.log('double');
+			parseDouble(instruction);
 			break;
 		case 'jump':
-			console.log('jump');
+			parseJump(instruction);
 			break;
 		default:
 			console.log('invalid instruction!');
 	}
+
 }
 
 // tests
